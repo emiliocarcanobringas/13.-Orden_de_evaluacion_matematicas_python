6e355ef2e8ce894d3ed35c21cb424ac33bba66f9
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-12-03 20:12:44 -0600

    Add files via upload

---
+++ 13.-Orden_de_evaluacion_matematicas_python.py
@@ -0,0 +1,14 @@
+# Este programa muestra el orden de evaluación matemática.
+
+print('Seguiremos el orden de la mnemotécnica "PEMDAS"')
+print("Paréntesis, exponentes,multiplicación,división,adición y sustracción")
+numero1 = 7
+numero2 = 3.1416
+ordendeevaluacion = (7*3.1416)**2*2/3
+print("El resultado de la operación es: ")
+print(ordendeevaluacion)
+print("El tipo de variable es: ")
+print(type(ordendeevaluacion))
+print("Este programa fue escrito por Emilio Carcaño Bringas")
+
+# Este programa fue escrito por Emilio Carcaño Bringas
